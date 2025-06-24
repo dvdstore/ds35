06589f9296a6e1d48b8eba1156ea0a0dd42b8157
dyeisley <dyeisley@redhat.com>
2025-06-24 15:49:42 -0400

    Created restock trigger.

--- mysqlds35/mysqlds35_create_all.sh
+++ mysqlds35/mysqlds35_create_all.sh
@@ -5,6 +5,7 @@ cd build
 perl mysqlds35_perl_create_db_tables_multi.pl $1 $2
 perl mysqlds35_perl_create_indexes_multi.pl $1 $2
 perl mysqlds35_perl_create_sp_multi.pl $1 $2
+perl mysqlds35_perl_create_trigger_multi.pl $1 $2
 cd ../load/
 perl ds35_create_mysql_multistore_load_files.pl $1 $2
 perl ds35_execute_mysql_multistore_load.pl $1 $2
